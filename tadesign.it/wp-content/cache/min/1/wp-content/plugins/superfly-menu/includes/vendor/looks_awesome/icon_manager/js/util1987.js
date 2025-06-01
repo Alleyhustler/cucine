a52b4d41c4d117b692af8a19922cea1a4de34b14
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2025-06-01 16:52:55 +0200

    f

---
+++ tadesign.it/wp-content/cache/min/1/wp-content/plugins/superfly-menu/includes/vendor/looks_awesome/icon_manager/js/util1987.js
@@ -0,0 +1,4 @@
+LAIconManagerUtil={getSet:function(string,delimeter){delimeter=typeof delimeter==='undefined'?'_####_':delimeter;var info=string.split(delimeter);if(info.length>1){return info[0]}
+return!1},getIcon:function(string,delimeter){delimeter=typeof delimeter==='undefined'?'_####_':delimeter;var info=string.split(delimeter);if(info.length>1){return info[1]}
+return!1},getIconClass:function(string,delimeter,prefix){delimeter=typeof delimeter==='undefined'?'_####_':delimeter;prefix=typeof prefix==='undefined'?'la':prefix;string=string.split('+').join(' ');var info=string.split(delimeter);if(info){var set=info[0];return prefix+md5(set)+'-'+info[1]}
+return!1}}
